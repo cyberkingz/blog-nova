9aae6f7b9bcc3b0164137364e9abe6176a847b2f
cyberkingz <thony.officiel@icloud.com>
2023-08-21 15:14:39 +0200

    update fr reviews

--- src/ComponentsFr/FifthProduct.js
+++ src/ComponentsFr/FifthProduct.js
@@ -17,13 +17,13 @@ function SecondProduct() {
     <div style={{ textAlign: 'center', padding: '0px', paddingBottom: '0px', width: '80%' }}>
       <div className='TextSection'>
       <h1 style={{textAlign: 'center', marginBottom: '0', paddingBottom: '0'}}>Dr. Dennis Gross</h1>
-      <h2 style={{textAlign: 'center', marginTop: '0', paddingTop: '0', marginBottom: '30px'}}>£577.85</h2>
+      <h2 style={{textAlign: 'center', marginTop: '0', paddingTop: '0', marginBottom: '30px'}}>680€</h2>
 
-      <p>This LED mask definitely brings the dermatologist’s office home. It looks so futuristic too, something my brother would liken to one of his futuristic shows.</p>
+      <p>Ce masque LED ramène vraiment le cabinet du dermatologue à la maison. Il a aussi un look très futuriste, quelque chose que mon frère comparerait à l'une de ses émissions futuristes.</p>
 
-      <p>We definitely liked this FDA-cleared wrinkle-fighting device, but we simply can’t justify the price. It costs £577.85. Not a typo.</p>
+      <p>Nous avons vraiment aimé cet appareil de lutte contre les rides approuvé par la FDA, mais nous ne pouvons tout simplement pas justifier le prix. Il coûte 680€. Ce n'est pas une erreur de frappe.</p>
 
-      <p>So, while it is a great LED mask, you can get the same perfecting results for your skin from Novalight for under $254.00</p><br/>
+      <p>Alors, bien que ce soit un excellent masque LED, vous pouvez obtenir les mêmes résultats parfaits pour votre peau avec Novalight pour moins de 300€</p><br/>
       
       </div>
 
@@ -42,4 +42,3 @@ function SecondProduct() {
 }
 
 export default SecondProduct
-
