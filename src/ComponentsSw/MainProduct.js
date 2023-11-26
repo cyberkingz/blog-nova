5d5fddb9a3fd0657761318548aba1026e7e00720
Anthony Arive <anthonyarive@Anthonys-MacBook-Air.local>
2023-11-26 17:04:49 +0100

    update sweden

--- src/ComponentsSw/MainProduct.js
+++ src/ComponentsSw/MainProduct.js
@@ -11,7 +11,7 @@ function MainProduct() {
 
         <div style={{ marginTop: '80px' }}>
           <p style={{ fontWeight: 'bold' }}>Kostnad</p>
-          <p>Vi fick bra resultat med LED-masker som kostade mindre än 2097.25 SEK. Faktum är att vårt toppval var det billigaste alternativet!</p>
+          <p>Vi fick bra resultat med LED-masker som kostade mindre än 2500 SEK. Faktum är att vårt toppval var det billigaste alternativet!</p>
           <Divider />
         </div>
 
@@ -31,7 +31,7 @@ function MainProduct() {
       <div className='imgTextWrapper' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', alignContent: 'center', gap: '30px', width: '100%', margin: 'auto auto' }}>
         <div className='img-container'>
           <img src={IMG} alt="" width={'50%'} className='productIMG' style={{ borderRadius: '20px', boxShadow: '0 0 10px 1px rgb(221 221 221)' }} />
-        <h2 style={{background: '#818cf8', color: 'white', width: 'fit-content', margin: '15px auto', borderRadius: '25px', paddingLeft: '20px', paddingRight: '20px'}}>2393,78 SEK</h2>
+        <h2 style={{background: '#818cf8', color: 'white', width: 'fit-content', margin: '15px auto', borderRadius: '25px', paddingLeft: '20px', paddingRight: '20px'}}>2405,00 SEK</h2>
         </div>
 
         <div style={{ textAlign: 'left', padding: '20px', paddingBottom: '0px' }}>
@@ -40,7 +40,7 @@ function MainProduct() {
 
             <p>Vi älskade verkligen Regenmask, en produkt som har fått en stor följande. Just nu är den dessutom 50% rabatterad, vilket gör den till den mest prisvärda av LED-maskerna som du kan använda hemma. Den här masken kan rensa porer och rena huden samtidigt som den förhindrar att tecken på förtida åldrande tar över. Den har också en del som täcker din hals, ett av de mest förbisedda områdena för åldrande hud.</p>
 
-            <p>Vi använde den varje dag i 20 minuter per dag, som Novalight föreslår att man gör mellan 15 och 30 minuter per dag. Det var enkelt och bekvämt, samtidigt som den arbetade med att bli av med tecken på solskador genom åren. Och det är ingen överraskning att den fungerar så bra... Den har LED-teknik som används av NASA för att hjälpa dig att hantera pigmentering, rodnad, torr hud och rynkor. Slutligen kunde vi inte sluta beundra hur strålande, felfri och vacker den lämnade vår hud! Resultaten är verkligen av professionell kvalitet. Och med 50% rabatt innebär det att den bara kostar 2207,52 SEK. Men skynda dig, du måste beställa den idag för att få den till det här fantastiskt låga priset.</p>
+            <p>Vi använde den varje dag i 20 minuter per dag, som Novalight föreslår att man gör mellan 15 och 30 minuter per dag. Det var enkelt och bekvämt, samtidigt som den arbetade med att bli av med tecken på solskador genom åren. Och det är ingen överraskning att den fungerar så bra... Den har LED-teknik som används av NASA för att hjälpa dig att hantera pigmentering, rodnad, torr hud och rynkor. Slutligen kunde vi inte sluta beundra hur strålande, felfri och vacker den lämnade vår hud! Resultaten är verkligen av professionell kvalitet. Och med 50% rabatt innebär det att den bara kostar 2 405 SEK. Men skynda dig, du måste beställa den idag för att få den till det här fantastiskt låga priset.</p>
           </div>
 
           <div className="ListSection" style={{marginTop: '30px', display: 'flex', flexDirection: 'column', gap: '10px'}}>
@@ -66,7 +66,7 @@ function MainProduct() {
             </Typography>
             <Typography component="div">
               <Box sx={{ display: 'flex', alignItems: 'center', fontSize: '14px' }}>
-                <Check sx={{ color: 'green', marginRight: '5px' }} /> För närvarande tillgänglig till ett rabatterat pris av 2393,78 SEK.
+                <Check sx={{ color: 'green', marginRight: '5px' }} /> För närvarande tillgänglig till ett rabatterat pris av 2405,00 SEK.
               </Box>
             </Typography>
           </div>
