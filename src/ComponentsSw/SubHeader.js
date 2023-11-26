f6c4bc64d7137bf577b0478b9e32c6ff1eb223cc
Anthony Arive <anthonyarive@Anthonys-MacBook-Air.local>
2023-11-26 17:07:56 +0100

    update subheader sw

--- src/ComponentsSw/SubHeader.js
+++ src/ComponentsSw/SubHeader.js
@@ -4,7 +4,7 @@ import './SubHeader.css'
 function SubHeader() {
   return (
     <div className='SubHeader' style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundColor: '#818cf8', padding: '10px', color: 'white'}}>
-        <h2>We Spent the Last 3 Months Testing the Best-Selling Light Therapy Masks</h2>
+        <h2>Vi har tillbringat de senaste 3 månaderna med att testa de mest sålda ljusbehandlingsmaskerna</h2>
     </div>
   )
 }
