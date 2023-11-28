c13daa8dfc925ecc6595ad351d6a5ef0b5cd2b95
Anthony Arive <anthonyarive@Anthonys-MacBook-Air.local>
2023-11-28 13:21:00 +0100

    update no

--- src/ComponentsNo/MainProduct.js
+++ src/ComponentsNo/MainProduct.js
@@ -31,7 +31,7 @@ function MainProduct() {
       <div className='imgTextWrapper' style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', alignContent: 'center', gap: '30px', width: '100%', margin: 'auto auto' }}>
         <div className='img-container'>
           <img src={IMG} alt="" width={'50%'} className='productIMG' style={{ borderRadius: '20px', boxShadow: '0 0 10px 1px rgb(221 221 221)' }} />
-        <h2 style={{background: '#818cf8', color: 'white', width: '160px', margin: '15px auto', borderRadius: '25px', paddingLeft: '10px', paddingRight: '10px'}}>2455 NOK</h2>
+        <h2 style={{background: '#818cf8', color: 'white', width: '160px', margin: '15px auto', borderRadius: '25px', paddingLeft: '10px', paddingRight: '10px'}}>2462 NOK</h2>
         </div>
 
         <div style={{ textAlign: 'left', padding: '20px', paddingBottom: '0px' }}>
@@ -40,7 +40,7 @@ function MainProduct() {
 
             <p>Vi likte virkelig Regenmask, en produkt som har fått en stor følging. Akkurat nå er den også priset med 50% rabatt, noe som gjør den til den mest kostnadseffektive av LED-maskene du kan bruke hjemme. Denne masken kan rense porene og rense huden samtidig som den forhindrer at tegn på tidlig aldring tar over. Den har også en del som dekker halsen din, et av de mest oversette områdene for aldrende hud.</p>
 
-            <p>Vi brukte den hver dag i 20 minutter per dag, slik Novalight anbefaler, som var mellom 15 og 30 minutter per dag. Det var enkelt og praktisk, samtidig som den jobbet med å bli kvitt tegn på solskader gjennom årene. Og det er ingen overraskelse at den fungerer så bra... Den har LED-teknologi som brukes av NASA for å hjelpe deg med å håndtere pigmentering, rødhet, tørr hud og rynker. Til slutt kunne vi ikke slutte å beundre hvor strålende, feilfri og vakker den etterlot huden vår! Resultatene er virkelig av profesjonell kvalitet. Og med 50% rabatt betyr det at den bare koster 2455 NOK. Men skynd deg, du må bestille den i dag for å få den til denne utrolig lave prisen.</p>
+            <p>Vi brukte den hver dag i 20 minutter per dag, slik Novalight anbefaler, som var mellom 15 og 30 minutter per dag. Det var enkelt og praktisk, samtidig som den jobbet med å bli kvitt tegn på solskader gjennom årene. Og det er ingen overraskelse at den fungerer så bra... Den har LED-teknologi som brukes av NASA for å hjelpe deg med å håndtere pigmentering, rødhet, tørr hud og rynker. Til slutt kunne vi ikke slutte å beundre hvor strålende, feilfri og vakker den etterlot huden vår! Resultatene er virkelig av profesjonell kvalitet. Og med 50% rabatt betyr det at den bare koster 2462 NOK. Men skynd deg, du må bestille den i dag for å få den til denne utrolig lave prisen.</p>
           </div>
 
           <div className="ListSection" style={{marginTop: '30px', display: 'flex', flexDirection: 'column', gap: '10px'}}>
@@ -66,7 +66,7 @@ function MainProduct() {
             </Typography>
             <Typography component="div">
               <Box sx={{ display: 'flex', alignItems: 'center', fontSize: '14px' }}>
-                <Check sx={{ color: 'green', marginRight: '5px' }} /> For øyeblikket tilgjengelig til en rabattert pris på 2455 NOK.
+                <Check sx={{ color: 'green', marginRight: '5px' }} /> For øyeblikket tilgjengelig til en rabattert pris på 2462 NOK.
               </Box>
             </Typography>
           </div>
